171f94b3279d04280a2bf77e44a1316d98802f3a
mlmiltchev <34401309+mlmiltchev@users.noreply.github.com>
2018-12-05 21:29:32 -0600

    Fixing more bugs

--- Group_Project_3/src/main/ParkButton.java
+++ Group_Project_3/src/main/ParkButton.java
@@ -34,8 +34,8 @@ public class ParkButton extends GUIButton {
 	 */
 	@Override
 	public void handle(ActionEvent event) {
-		if (super.getCurrentState() == "BRAKING"
-				|| (super.getCurrentSpeed() == 0 && super.getCurrentState() != "OFF")) {
+		if (super.getCurrentSpeed() == 0
+				&& (super.getCurrentState() == "BRAKING" || super.getCurrentState() != "OFF")) {
 			VehicleContext.instance().changeState(VehicleParkState.instance());
 		}
 		// Vehicle can only be PARKED when brake is applied and speed is 0
